bcc3d8f85928dbc67c122c239ad32924ba229d64
Neriya Rosner <neri.coder@gmail.com>
2023-07-29 20:09:04 +0300

    map to object

--- src/app/editor/canvas/modules/shapes/ShapesToComponentMap.ts
+++ src/app/editor/canvas/modules/shapes/ShapesToComponentMap.ts
@@ -1,5 +1,13 @@
 import { Square, SQUARE_SHAPE_TYPE } from "@/editor/canvas/modules/shapes/square"
+import { Rectangle, RECTANGLE_SHAPE_TYPE } from "@/editor/canvas/modules/shapes/rectangle"
 
 export const ShapesToComponentMap = {
-    [SQUARE_SHAPE_TYPE]: Square,
+    [SQUARE_SHAPE_TYPE]: {
+        component: Square,
+        name: "Square",
+    },
+    [RECTANGLE_SHAPE_TYPE]: {
+        component: Rectangle,
+        name: "Rectangle",
+    },
 }
