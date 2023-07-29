ac1d1c996c59d0c23d18c603f85cbe5cebfa8b7e
Neriya Rosner <neri.coder@gmail.com>
2023-07-29 15:53:14 +0300

    display shape

--- src/app/(canvas)/(pages)/canvas/page.tsx
+++ src/app/(canvas)/(pages)/canvas/page.tsx
@@ -1,11 +1,15 @@
 import { Canvas } from "@/app/(canvas)/components/Canvas"
 import styles from "./canvas.module.css"
+import { Square } from "@/app/(canvas)/components/Shapes/Square"
 
 export default function() {
 
     return (
         <div className={styles.container}>
-            <Canvas>
+            <Canvas items={[
+                <Square color={"red"} size={10} y={10} x={10} />,
+            ]}>
+
             </Canvas>
         </div>
     )
