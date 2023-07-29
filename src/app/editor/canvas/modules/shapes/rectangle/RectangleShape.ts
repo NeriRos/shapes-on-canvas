b19201062d97219c1f0c1fc0070f5bb71d80292d
Neriya Rosner <neri.coder@gmail.com>
2023-07-29 21:16:49 +0300

    correct type

--- src/app/editor/canvas/modules/shapes/rectangle/RectangleShape.ts
+++ src/app/editor/canvas/modules/shapes/rectangle/RectangleShape.ts
@@ -3,7 +3,7 @@ import { Shape } from "@/editor/canvas/components/shape/Shape"
 export const RECTANGLE_SHAPE_TYPE = "rectangle"
 
 export type RectangleShape = {
-    type: SQUARE_SHAPE_TYPE
+    type: RECTANGLE_SHAPE_TYPE
     attributes: {
         width: number
         height: number
