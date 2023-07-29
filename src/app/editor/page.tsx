76fa240d98f70f90ba54ee426a04d506ff956ebd
Neriya Rosner <neri.coder@gmail.com>
2023-07-29 19:32:34 +0300

    create canvas click listener

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,9 +1,17 @@
 import React from "react"
 import { FloatingMenu } from "@/editor/coordinates/components/floating-menu/FloatingMenu"
 import { CanvasContainer } from "@/editor/canvas/components/CanvasContainer"
+import { CanvasClickListener } from "@/editor/canvas/modules/gestures-detector/components/CanvasClickListener"
 
 export default function() {
     return <CanvasContainer>
         <FloatingMenu />
+        <CanvasClickListener shape={{
+            type: "square",
+            color: "red",
+            attributes: {
+                size: 120,
+            },
+        }} />
     </CanvasContainer>
 }
