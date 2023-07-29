ac4ab1487269345596bb0665fdf098591e309c7f
Neriya Rosner <neri.coder@gmail.com>
2023-07-29 11:17:30 +0300

    move under src

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/index.module.css"
+
+export default function() {
+    return (
+        <div className={styles.container}>
+            <h1>Shapes on Canvas</h1>
+        </div>
+    )
+}
