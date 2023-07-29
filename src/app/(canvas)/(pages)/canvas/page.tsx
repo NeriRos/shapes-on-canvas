e1b5ddda22fc6d972b7eb58380e86e9a617d9e72
Neriya Rosner <neri.coder@gmail.com>
2023-07-29 17:38:42 +0300

    use provider

--- src/app/(canvas)/(pages)/canvas/page.tsx
+++ src/app/(canvas)/(pages)/canvas/page.tsx
@@ -1,17 +1,27 @@
-import { Canvas } from "@/app/(canvas)/components/Canvas"
 import styles from "./canvas.module.css"
-import { Square } from "@/app/(canvas)/components/Shapes/Square"
 import { FloatingMenu } from "@/app/(canvas)/components/FloatingMenu/FloatingMenu"
+import { CanvasProvider } from "@/app/(canvas)/components/Canvas/CanvasProvider"
 
 export default function() {
 
     return (
         <div className={styles.container}>
-            <Canvas items={[
-                <Square color={"red"} size={10} y={10} x={10} />,
+            <CanvasProvider shapes={[
+                {
+                    id: 1,
+                    type: "square",
+                    position: {
+                        x: 10,
+                        y: 10,
+                    },
+                    color: "red",
+                    attributes: {
+                        size: 10,
+                    },
+                },
             ]}>
                 <FloatingMenu />
-            </Canvas>
+            </CanvasProvider>
         </div>
     )
 }
