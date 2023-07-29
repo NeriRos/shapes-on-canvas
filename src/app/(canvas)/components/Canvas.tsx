93203267f0463d9477ce559774d5391adeddc1c1
Neriya Rosner <neri.coder@gmail.com>
2023-07-29 15:04:11 +0300

    css dimensions

--- src/app/(canvas)/components/Canvas.tsx
+++ src/app/(canvas)/components/Canvas.tsx
@@ -2,16 +2,14 @@
 
 import styles from "./Canvas.module.css"
 import { ReactNode, useRef } from "react"
-import { useWindowDimensions } from "@/core/hooks/useWindowDimensions"
 
 export type CanvasProps = {
     children?: ReactNode
 }
 export const Canvas = (props: CanvasProps) => {
     const canvasRef = useRef(null)
-    const dimensions = useWindowDimensions()
 
-    return <canvas ref={canvasRef} className={styles.canvas} width={dimensions.width} height={dimensions.height}>
+    return <canvas ref={canvasRef} className={styles.canvas}>
         {props.children}
     </canvas>
 }
