d17f6dba4c148ad0a03f17d65cc2f4a2279dc57b
Neriya Rosner <neri.coder@gmail.com>
2023-07-30 20:00:27 +0300

    function name

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,7 +2,7 @@ import styles from "./index.module.css"
 import Link from "next/link"
 import { GET_STARTED_URL, TEXTS } from "./consts"
 
-export default function() {
+export default function Page() {
     return (
         <div className={styles.container}>
             <h1>Shapes on Canvas</h1>
