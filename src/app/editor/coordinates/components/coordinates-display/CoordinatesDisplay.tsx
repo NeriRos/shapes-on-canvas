949649178d61ca07e6ed5ca2d871e87315402213
Neriya Rosner <neri.coder@gmail.com>
2023-07-30 14:09:20 +0300

    keep shape

--- src/app/editor/coordinates/components/coordinates-display/CoordinatesDisplay.tsx
+++ src/app/editor/coordinates/components/coordinates-display/CoordinatesDisplay.tsx
@@ -1,21 +1,32 @@
 import { MouseMoveListener } from "@/editor/canvas/modules/gestures-detector/components/MouseMoveListener"
 import Styles from "./CoordinatesDisplay.module.css"
 import { useState } from "react"
+import { Shape } from "@/editor/canvas/components/shape/Shape"
 
-export type CoordinatesDisplayProps = {
-    itemClassName?: string
-}
+export type CoordinatesDisplayProps = {}
 
 export const CoordinatesDisplay = (props: CoordinatesDisplayProps) => {
     const [coordinates, setCoordinates] = useState({ x: 0, y: 0 })
+    const [shape, setShape] = useState<Shape>()
+
+    const onMove = (position: Shape["position"]) => {
+        setCoordinates(position)
+    }
+
+    const onShapeChange = (shape: Shape) => {
+        setShape(shape)
+    }
 
     return <>
+        <div className={Styles.title}>
+            {shape?.title}
+        </div>
         <div className={Styles.coordinateContainer}>
             Mouse X - {coordinates.x}
         </div>
         <div className={Styles.coordinateContainer}>
             Mouse Y - {coordinates.y}
         </div>
-        <MouseMoveListener action={setCoordinates} />
+        <MouseMoveListener onMove={onMove} onShapeChange={onShapeChange} />
     </>
 }
