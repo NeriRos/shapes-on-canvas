77b48e4dd591bf1ad5444442f71d8b7a6dc08153
Neriya Rosner <neri.coder@gmail.com>
2023-07-30 18:51:57 +0300

    cache draw

--- src/app/editor/canvas/modules/shapes/rectangle/Rectangle.tsx
+++ src/app/editor/canvas/modules/shapes/rectangle/Rectangle.tsx
@@ -2,21 +2,22 @@
 
 import { RectangleShape } from "@/editor/canvas/modules/shapes/rectangle/RectangleShape"
 import { useShape } from "@/editor/canvas/modules/shapes/hooks/useShape"
+import { useCallback } from "react"
 
 export type RectangleProps = {
-    redrawTimes: number,
     shape: RectangleShape
 }
 
-export const Rectangle = ({ shape, redrawTimes }: RectangleProps) => {
+export const Rectangle = ({ shape }: RectangleProps) => {
+    const draw = useCallback((context: CanvasRenderingContext2D) => {
+        if (shape.color)
+            context.fillStyle = shape.color
+        context.fillRect(shape.position.x, shape.position.y, shape.attributes.width, shape.attributes.height)
+    }, [shape])
+
     useShape({
-        redrawTimes,
         shape,
-        draw: (context) => {
-            if (shape.color)
-                context.fillStyle = shape.color
-            context.fillRect(shape.position.x, shape.position.y, shape.attributes.width, shape.attributes.height)
-        },
+        draw,
     })
 
     return null
