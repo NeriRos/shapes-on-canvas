70d2ee534664f74a176760d1e4fa3fc9ef138cbf
Neriya Rosner <neriyarosner@gmail.com>
2023-08-06 15:06:24 +0300

    design

--- src/core/components/floating-credits/FloatingCredits.tsx
+++ src/core/components/floating-credits/FloatingCredits.tsx
@@ -3,7 +3,9 @@ import styles from "./FloatingCredits.module.css"
 
 export const FloatingCredits = () => {
     return <div className={styles.credits}>
+        <span>
         Neriya Rosner © 2023 | <Link href={"https://github.com/NeriRos/shapes-on-canvas"}>GitHub</Link> | <Link
-        href={"https://www.linkedin/in/neriya-rosner"}>LinkedIn</Link>
+            href={"https://www.linkedin.com/in/neriya-rosner"}>LinkedIn</Link>
+        </span>
     </div>
 }
