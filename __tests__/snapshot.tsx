209ded8447ac55b08c3ed7c7fa85227bc251275d
Neriya Rosner <neri.coder@gmail.com>
2023-08-13 11:03:16 +0300

    remove test

--- __tests__/snapshot.tsx
+++
@@ -1,7 +0,0 @@
-import Home from "@/src/app/(marketing)/page"
-import { render } from "@testing-library/react"
-
-it("renders homepage unchanged", () => {
-    const { container } = render(<Home />)
-    expect(container).toMatchSnapshot()
-})
